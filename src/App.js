04c5b06e08d909a206ef964e5fc6eaa282b5adaa
Matthew Read <mattread90@gmail.com>
2020-11-28 19:07:45 +0000

    Initialise firebase app and auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import firebase from 'firebase/app'
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './logo.svg'
+import './App.css'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBi61brUhtspSb67GCblNm-2OJbg9zIMoE',
+  authDomain: 'quiz-13ac3.firebaseapp.com',
+  databaseURL: 'https://quiz-13ac3.firebaseio.com',
+  projectId: 'quiz-13ac3',
+  storageBucket: 'quiz-13ac3.appspot.com',
+  messagingSenderId: '762624056836',
+  appId: '1:762624056836:web:b3f99cf9e928ec27ed9f51',
+  measurementId: 'G-V0Q5LX0677',
 }
 
-export default App;
+const authConfig = {
+  signInFlow: 'popup',
+  signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+    // User successfully signed in.
+    // Return type determines whether we continue the redirect automatically
+    // or whether we leave that to developer to handle.
+    return false
+  },
+  signInOptions: [
+    // List of OAuth providers supported.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+  // Other config options...
+}
+
+firebase.initializeApp(firebaseConfig)
+const ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(setUser)
+  }, [])
+  useEffect(() => {
+    if (ui.isPendingRedirect()) {
+      ui.start('#firebaseui-auth-container', authConfig)
+    }
+  }, [])
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          {user ? (
+            <>Welcome back, {user.displayName}</>
+          ) : (
+            <>Sign in to continue</>
+          )}
+        </p>
+        <div id='firebaseui-auth-container'></div>
+      </header>
+    </div>
+  )
+}
+
+export default App
